18e4d3a382a9b116a37f256e46ec663e64c38152
wommy <tom@technomad.media>
2022-03-16 00:56:09 -0400

    from 10/06/21

---
+++ index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+const pkg = require('./package.json')
+
+console.log(`
+	name: ${pkg.name}
+	version: ${pkg.version}
+	description: ${pkg.description}
+`)
+
+console.log(`wommy`)
