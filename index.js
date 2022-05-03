fc19e84da01fec239922b0b170614ed1672d3597
wommy <tom@technomad.media>
2022-05-03 02:53:19 -0400

    bump content

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ console.log(`
 	name: ${pkg.name}
 	version: ${pkg.version}
 	description: ${pkg.description}
-`)
 
-console.log(`wommy`)
+	About
+	---
+	  my name is Tommy, i was born in 1990
+	i make websites, mostly using 
+	    Static Site Generators (SSGs)
+	
+	rd
+	---
+	TUDO:
+	  - [ ] fetch \`wommy/rd\`
+	  - [ ] ? fetch \`tudo\`
+	
+	program structure
+	---
+	- fetch \`~/rd/22/05-may\`
+`)
