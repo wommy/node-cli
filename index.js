aef3a1f9930b9a98349dfaebb1c2b3f88187f8ae
wommy <tom@technomad.media>
2022-05-03 04:05:27 -0400

    pre-mv

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 #!/usr/bin/env node
 
 const pkg = require('./package.json')
+const path_i3r0y = './rd/22/03-14--i3r0y.md'
+// import { readFile } from 'fs'
+const readFile = require('fs').readFile
+
+const i3r0y = await readFile(path_i3r0y, 'utf8', (err, data) => {
+	if (err) throw err
+	return data
+})
 
 console.log(`
 	name: ${pkg.name}
@@ -22,4 +30,10 @@ console.log(`
 	program structure
 	---
 	- fetch \`~/rd/22/05-may\`
+	  fake fetched it,
+		REVISIT
+	- load contents?
+
+	${i3r0y}
+	
 `)
